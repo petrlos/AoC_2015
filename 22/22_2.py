777282f753d4ca84a5d2d1b9b1ac977deee8e24f
Petr Los <petrlos@gmail.com>
2023-02-24 14:11:19 +0100

    Day 22: Class working, no queue

--- 22/22_2.py
+++ 22/22_2.py
@@ -1,25 +1,22 @@
 # Advent of Code 2015: Day 22
 import re
-from collections import deque
-from copy import deepcopy
+from queue import PriorityQueue
 
 class Game_state:
-    def __init__(self, mage_HP, mage_MP, boss_HP, boss_DMG):
+    def __init__(self, mage_HP, mage_MP, boss_HP, boss_DMG, active_effects):
         self.mage_HP = mage_HP
         self.mage_MP = mage_MP
         self.boss_HP = boss_HP
         self.boss_DMG = boss_DMG
         self.shield_on = False
         self.MP_consumed = 0
-        self.active_effects = deque()
+        self.active_effects = active_effects
+        self.spells_casted = []
 
     def __str__(self):
-        effects = ""
-        for effect in self.active_effects:
-            effects += str(effect)
         return "Mage: {0} HP / {1} MP; Boss {2} HP / {3} DMG"\
             .format(self.mage_HP, self.mage_MP, self.boss_HP, self.boss_DMG) + "\n" + \
-            "  {0}".format(effects) + "\n" + \
+            "  {0}".format(", ".join(self.active_effects)) + "\n" + \
             "  Total MP Consumed: {0}".format(self.MP_consumed)
 
     @property
@@ -28,41 +25,36 @@ class Game_state:
 
     @property
     def mage_dead(self):
-        return self.mage_HP <= 0
+        return (self.mage_HP <= 0) or (self.mage_MP < 53)
 
     @property
-    def shield_bonus(self):
+    def shield_bonus(self): # if shield on
         return 7 if self.shield_on else 0
 
     def add_spell_to_effects(self, spell_name):
-        atributes = spells[spell_name]
-        if atributes["cost"] < self.mage_MP:
-            if spell_name in ["magic_misile", "drain"]: #instant cast, no effect
-                self.mage_MP -= atributes["cost"]
-                self.MP_consumed += atributes["cost"]
-                self.boss_HP -= atributes["dmg"]
-                self.mage_HP += atributes["heal"]
-            else:
-                active_spell_names = [spell[0] for spell in self.active_effects]
-                if spell_name not in active_spell_names:
-                    self.active_effects.append((spell_name,atributes["max_time"]))
-                    self.mage_MP -= atributes["cost"]
-                    self.MP_consumed += atributes["cost"]
+        self.active_effects += [spell_name]*spells[spell_name]["max_time"]
 
-    def cast_effects(self):
-        self.shield_on = False
-        new_active_effects = []
-        for spell_name, timer in self.active_effects:
-            timer -= 1
-            if spell_name == "shield":
-                self.shield_on = True
-            elif spell_name == "poison":
-                self.boss_HP -= spells[spell_name]["dmg"]
-            elif spell_name == "recharge":
-                self.mage_MP += spells[spell_name]["mana_recharge"]
-            if timer > 0:
-                new_active_effects.append((spell_name, timer))
-        self.active_effects = deepcopy(new_active_effects)
+    def cast_effects(self): #cast current effects, after cast effect removed
+        for effect in set(self.active_effects): #each effect only once
+            self.boss_HP -= spells[effect]["dmg"]
+            self.mage_MP += spells[effect]["mana_recharge"]
+            self.active_effects.remove(effect)
+
+    def mage_turn(self, spell_name):
+        self.cast_effects()
+        self.mage_MP -= spells[spell_name]["cost"]
+        self.MP_consumed += spells[spell_name]["cost"]
+        if spell_name in ["magic_misile", "drain"]:
+            self.mage_HP += spells[spell_name]["heal"]
+            self.boss_HP -= spells[spell_name]["dmg"]
+        else:
+            self.add_spell_to_effects(spell_name)
+
+    def boss_turn(self):
+        self.shield_on = "shield" in self.active_effects
+        self.cast_effects()
+        if not self.boss_dead:
+            self.mage_HP -= (self.boss_DMG-self.shield_bonus)
 
 def define_spells(lines):
     reg_spell_name = re.compile(r"\w+")
@@ -75,43 +67,19 @@ def define_spells(lines):
         spells[spell_name] = dict(zip(parameters, numbers))
     return spells
 
-def mage_attacks_boss(game, lvl=1):
-    queue = deque([game])
-
-    lowest_mana_consumed = 9999
-    while queue:
-        start_state = queue.popleft()
-        for spell_name, atributes in spells.items():
-            current_state = deepcopy(start_state)
-            #player turn - apply effects
-            if lvl == 2:
-                current_state.mage_HP = current_state.mage_HP - 1 #
-            current_state.cast_effects()
-            #player turn - cast spell
-            current_state.add_spell_to_effects(spell_name)
-            #boss turn - apply effect
-            current_state.cast_effects()
-            if current_state.boss_dead:  # boss dead
-                print(current_state.MP_consumed)
-                if current_state.MP_consumed < lowest_mana_consumed:
-                    lowest_mana_consumed = current_state.MP_consumed
-                continue
-            # boss attack:
-            boss_hit = current_state.boss_DMG - current_state.shield_bonus
-            if boss_hit < 1:
-                boss_hit = 1
-            current_state.mage_HP -= boss_hit
-            if current_state.mage_dead:  # mage dead
-                continue
-            else:  # both live
-                queue.append(deepcopy(current_state))
-    return lowest_mana_consumed
-
 # MAIN
 with open("spells.txt") as file:
     lines = file.read().splitlines()
 
 spells = define_spells(lines)
-game_start = Game_state(50, 500, 51, 9) #mageHP, mageMP, bossHP, bossDMG
 
-print(mage_attacks_boss(game_start, 2))
+game_start = Game_state(10, 250, 14, 8, []) #mageHP, mageMP, bossHP, bossDM
+
+#spells_queue = "poison,magic_misile".split(",")
+spells_queue = "recharge,shield,drain,poison,magic_misile".split(",")
+
+for spell_casted in spells_queue:
+    game_start.mage_turn(spell_casted)
+    game_start.boss_turn()
+
+print(game_start)
